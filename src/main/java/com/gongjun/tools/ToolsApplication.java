0f47bf3128b7006ef8a26a399dd8a0926bf64148
GongJun <28402674@qq.com>
2019-01-17 12:46:56 +0800

    添加Swagger

--- src/main/java/com/gongjun/tools/ToolsApplication.java
+++ src/main/java/com/gongjun/tools/ToolsApplication.java
@@ -2,13 +2,14 @@ package com.gongjun.tools;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.gongjun.tools"})
 @MapperScan(basePackages = {"com.gongjun.tools.mapper"})
+@EnableAutoConfiguration
 public class ToolsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gongjun/tools/controller/ABCController.java
+++ src/main/java/com/gongjun/tools/controller/ABCController.java
@@ -2,6 +2,7 @@ package com.gongjun.tools.controller;
 
 import com.gongjun.tools.model.Test;
 import com.gongjun.tools.service.ITestService;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +21,31 @@ import java.util.List;
  * 例如：本来应该到success.jsp页面的，则其显示success.
  * 2)如果需要返回到指定页面，则需要用 @Controller配合视图解析器InternalResourceViewResolver才行。
  * 3)如果需要返回JSON，XML或自定义mediaType内容到页面，则需要在对应的方法上加上@ResponseBody注解。
- * */
-/**
+ **/
+/*
  * 定义控制层采用@RestController/@Controller
- * */
+ */
 @RestController
 @RequestMapping("")
+@Api(value = "/", description = "测试接口")
 public class ABCController {
     @Resource
     private ITestService service;
+
     @GetMapping("")
-    public String ABC(){
+    public String ABC() {
         List<Test> tests = this.service.selectList(null);
-        System.out.println(null == tests?null:tests.size());
+        System.out.println(null == tests ? null : tests.size());
         return "hello";
+    }
+    /**
+     * @param [aa, bb]
+     * @description 方法注解采用"/m"然后tab键即可
+     * @author GongJun
+     * @time 2019/1/17 11:03
+     * @return java.lang.String
+     **/
+    public String ss(String aa, String bb) {
+        return aa + bb;
     }
 }
