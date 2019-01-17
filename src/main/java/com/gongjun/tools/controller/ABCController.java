54640c065d18b987d20b63abd2a9c6f604573161
GongJun <28402674@qq.com>
2019-01-17 09:37:15 +0800

    注解调整

--- src/main/java/com/gongjun/tools/controller/ABCController.java
+++ src/main/java/com/gongjun/tools/controller/ABCController.java
@@ -2,17 +2,17 @@ package com.gongjun.tools.controller;
 
 import com.gongjun.tools.model.Test;
 import com.gongjun.tools.service.ITestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 @RequestMapping("")
 public class ABCController {
-    @Autowired
+    @Resource
     private ITestService service;
     @GetMapping("")
     public String ABC(){
--- src/main/java/com/gongjun/tools/service/impl/TestServiceImpl.java
+++ src/main/java/com/gongjun/tools/service/impl/TestServiceImpl.java
@@ -4,12 +4,13 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.gongjun.tools.mapper.TestMapper;
 import com.gongjun.tools.model.Test;
 import com.gongjun.tools.service.ITestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class TestServiceImpl  extends ServiceImpl<TestMapper,Test> implements ITestService{
-    @Autowired
+    @Resource
     private TestMapper mapper;
     public void ss(){
         int a = this.mapper.selectList(null)==null?0:this.mapper.selectCount(null);
