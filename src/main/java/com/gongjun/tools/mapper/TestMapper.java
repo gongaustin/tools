7efab922aa729f8f8000950475bfd7d164bcfbee
GongJun <28402674@qq.com>
2019-01-17 09:25:41 +0800

    mapper添加注解

--- src/main/java/com/gongjun/tools/mapper/TestMapper.java
+++ src/main/java/com/gongjun/tools/mapper/TestMapper.java
@@ -2,6 +2,8 @@ package com.gongjun.tools.mapper;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.gongjun.tools.model.Test;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface TestMapper extends BaseMapper<Test> {
 }
--- src/main/java/com/gongjun/tools/service/impl/TestServiceImpl.java
+++ src/main/java/com/gongjun/tools/service/impl/TestServiceImpl.java
@@ -4,8 +4,15 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.gongjun.tools.mapper.TestMapper;
 import com.gongjun.tools.model.Test;
 import com.gongjun.tools.service.ITestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestServiceImpl  extends ServiceImpl<TestMapper,Test> implements ITestService{
+    @Autowired
+    private TestMapper mapper;
+    public void ss(){
+        int a = this.mapper.selectList(null)==null?0:this.mapper.selectList(null).size();
+        System.out.println("a="+a);
+    }
 }
